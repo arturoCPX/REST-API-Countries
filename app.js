9e8c2f98fd14328e2fec42621a0a6a8b9491efa6
Arturo Coronado <arturo.cpx@gmail.com>
2025-05-11 22:35:45 -0700

    correction coments

--- app.js
+++ app.js
@@ -2,21 +2,18 @@ const express = require('express');
 const bodyParser = require('body-parser');
 const countryRoutes = require('./routes/countryRoutes');
 const languageRoutes = require('./routes/languageRoutes');
-// const currencyRoutes = require('./routes/currencyRoutes'); // Si creaste las rutas de moneda
 const errorHandler = require('./middleware/errorHandler');
 
 const app = express();
 const port = process.env.PORT || 3000;
 
-// Middleware para analizar el cuerpo de las peticiones
 app.use(bodyParser.json());
 
 // Rutas
 app.use('/api', countryRoutes);
 app.use('/api', languageRoutes);
-// app.use('/api', currencyRoutes); // Si creaste las rutas de moneda
 
-// Middleware de manejo de errores (debe ir después de las rutas)
+
 app.use(errorHandler);
 
 app.listen(port, () => {
